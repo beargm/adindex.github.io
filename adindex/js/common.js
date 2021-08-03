0f6d672fc800d8d67427030c80fcdf0cd2dd2609
Maksim <42744073+beargm@users.noreply.github.com>
2021-08-03 17:20:16 +0300

    first_commit

---
+++ adindex/js/common.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    AOS.init({
+        disable: 'mobile',
+        startEvent: 'DOMContentLoaded',
+        easing: 'ease',
+        duration: 2000,
+        offset: '0',
+        once: true
+    });
+
+    $('.lazy').lazy();
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
