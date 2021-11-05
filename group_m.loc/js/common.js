40832012eff747a2bdb857e6eb583726266a88ea
Maksim <42744073+beargm@users.noreply.github.com>
2021-11-05 22:12:45 +0200

    asd

--- group_m.loc/js/common.js
+++ group_m.loc/js/common.js
@@ -125,14 +125,7 @@ $(function () {
 
     //end mob menu
 
-    AOS.init({
-        disable: 'mobile',
-        startEvent: 'DOMContentLoaded',
-        easing: 'ease',
-        duration: 2000,
-        offset: '0',
-        once: true
-    });
+
 
 
 
